f78961041dc01495fabe342da8a9a193363d614b
Naima Hassan <naima.hassan434@gmail.com>
2022-05-27 00:02:12 +0100

    display tours

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./Loading";
 import Tours from "./Tours";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = "https://course-api.com/react-tours-project";
 
 function App() {
@@ -35,7 +34,7 @@ function App() {
   }
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} />
     </main>
   );
 }
